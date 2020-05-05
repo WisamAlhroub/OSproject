416840ea8bf9cb3db9fc6d1f16ddf36560a78c12
Mohammed-samaheen <smai2011643@gmail.com>
2020-05-05 16:08:43 +0300

    crate the project and attended the data

--- part1.py
+++ part1.py
@@ -2,7 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from util.Preprocessor import *
 
+# read the data and and put it in a table
 information, table = read_data()
 physical_mem_size, page_size, round_Q, CS = information
 
-
+# TODO sort the data by Arrival Time to make the table suitable for pressing
