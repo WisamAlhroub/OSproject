3d963872cf37090bff79a4b35108c67ec759dbf7
Mohammed-samaheen <smai2011643@gmail.com>
2020-05-05 15:58:49 +0300

    crate the project and attended the data

---
+++ part1.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from util.Preprocessor import *
+
+information, table = read_data()
+physical_mem_size, page_size, round_Q, CS = information
+
+
---
+++ util/Preprocessor.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+
+def read_data(file_name='processes.txt'):
+    with open('processes.txt', 'r+') as d:
+        content = d.readlines()
+
+    information = tuple(map(int, content[:4]))
+    pre_data = np.array(content[4:])
+    pre_data = pre_data[pre_data != '\n']
+    data = np.array([list(map(int,x.split('  '))) for x in pre_data])
+
+    table = pd.DataFrame(data=data, columns=['Process ID', 'Arrival Time',
+                                             'CPU Burst', 'Size in Bytes'])
+    return information, table
